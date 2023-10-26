f3447bfa49e7c98df161b885c47d62a5ad284101
basotyev <olzhas.sabi@gmail.com>
2023-10-26 15:43:44 +0600

    till 8.2 chapter

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -86,21 +86,32 @@ func (app *application) updateMovieHandler(w http.ResponseWriter, r *http.Reques
 		return
 	}
 	var input struct {
-		Title   string       `json:"title"`
-		Year    int32        `json:"year"`
-		Runtime data.Runtime `json:"runtime"`
-		Genres  []string     `json:"genres"`
+		Title   *string       `json:"title"`
+		Year    *int32        `json:"year"`
+		Runtime *data.Runtime `json:"runtime"`
+		Genres  []string      `json:"genres"`
 	}
+
 	err = app.readJSON(w, r, &input)
 	if err != nil {
 		app.logger.Println("wrong body params", err)
 		app.badRequestResponse(w, r, err)
 		return
 	}
-	movie.Title = input.Title
-	movie.Year = input.Year
-	movie.Runtime = input.Runtime
-	movie.Genres = input.Genres
+
+	if input.Title != nil {
+		movie.Title = *input.Title
+	}
+	// We also do the same for the other fields in the input struct.
+	if input.Year != nil {
+		movie.Year = *input.Year
+	}
+	if input.Runtime != nil {
+		movie.Runtime = *input.Runtime
+	}
+	if input.Genres != nil {
+		movie.Genres = input.Genres // Note that we don't need to dereference a slice.
+	}
 
 	v := validator.New()
 	if data.ValidateMovie(v, movie); !v.Valid() {
