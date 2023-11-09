bad10c3a8dbed27774e2aac44d60dd08e7200816
basotyev <olzhas.sabi@gmail.com>
2023-11-09 15:36:05 +0600

    till 13 chapter

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,12 +4,10 @@ import (
 	"context"
 	"database/sql"
 	"flag"
-	"fmt"
 	"github.com/basotyev/greenlight/internal/data"
+	"github.com/basotyev/greenlight/internal/jsonlog"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
 	_ "github.com/lib/pq"
-	"log"
-	"net/http"
 	"os"
 	"time"
 )
@@ -25,11 +23,16 @@ type config struct {
 		maxIdleConns int
 		maxIdleTime  string
 	}
+	limiter struct {
+		rps     float64
+		burst   int
+		enabled bool
+	}
 }
 
 type application struct {
 	config config
-	logger *log.Logger
+	logger *jsonlog.Logger
 	models data.Models
 }
 
@@ -45,15 +48,19 @@ func main() {
 	flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns", 25, "PostgreSQL max idle connections")
 	flag.StringVar(&cfg.db.maxIdleTime, "db-max-idle-time", "15m", "PostgreSQL max connection idle time")
 
+	flag.Float64Var(&cfg.limiter.rps, "limiter-rps", 2, "Rate limiter maximum requests per second")
+	flag.IntVar(&cfg.limiter.burst, "limiter-burst", 4, "Rate limiter maximum burst")
+	flag.BoolVar(&cfg.limiter.enabled, "limiter-enabled", true, "Enable rate limiter")
+
 	flag.Parse()
-	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+	logger := jsonlog.New(os.Stdout, jsonlog.LevelInfo)
+
 	db, err := openDB(cfg)
 	if err != nil {
-		logger.Fatal(err)
+		logger.PrintFatal(err, nil)
 	}
 	defer db.Close()
-	logger.Printf("database connection pool established")
-
+	logger.PrintInfo("database connection pool established", nil)
 	//migrationDriver, err := postgres.WithInstance(db, &postgres.Config{
 	//	DatabaseName: "greenlight",
 	//})
@@ -68,7 +75,7 @@ func main() {
 	//if err != nil && !errors.Is(err, migrate.ErrNoChange) {
 	//	logger.Fatal(err, nil)
 	//}
-	logger.Printf("database migrations applied")
+	logger.PrintInfo("database migrations applied", nil)
 
 	app := application{
 		config: cfg,
@@ -76,17 +83,10 @@ func main() {
 		models: data.NewModels(db),
 	}
 
-	srv := &http.Server{
-		Addr:         fmt.Sprintf(":%d", app.config.port),
-		Handler:      app.routes(),
-		ReadTimeout:  10 * time.Second,
-		WriteTimeout: 30 * time.Second,
-		IdleTimeout:  time.Minute,
+	err = app.serve()
+	if err != nil {
+		logger.PrintFatal(err, nil)
 	}
-
-	logger.Println(fmt.Sprintf("Server is running on port %s", srv.Addr))
-	err = srv.ListenAndServe()
-	logger.Fatal(err)
 }
 
 func openDB(cfg config) (*sql.DB, error) {
